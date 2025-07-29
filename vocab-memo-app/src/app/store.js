8b450f8138e6d0d812914d873a59f8bc68453307
Nahom ketsela <nahoket@gmail.com>
2025-07-29 17:13:49 +0300

    fixed some bugs.

--- vocab-memo-app/src/app/store.js
+++ vocab-memo-app/src/app/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import vocabularyReducer from '../features/vocabulary/vocabularySlice';
+import testReducer from '../features/test/testSlice'; 
 
 export const store = configureStore({
   reducer: {
     vocabulary: vocabularyReducer,
+    test: testReducer, 
   },
 });
-// This file sets up the Redux store for the vocabulary memo application,
